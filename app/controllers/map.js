983546bb559818487651c08764d7e090c93865a0
Jeroen Valcke <valcke_jeroen@hotmail.com>
2018-02-07 17:31:45 +0100

    Setup API specifications through swagger

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,5 +1,34 @@
 "use strict";
 
+/**
+ * @api {POST} /api/1.0.0/projects-am/map Get filtered projects for the map.
+ * @apiGroup Map
+ * @apiVersion 1.0.0
+ *
+ * @apiParam (body) {Object} MapBodyQuery query object
+ * @apiParam (body) {String} MapBodyQuery.q search query
+ * @apiParam (body) {String[]} MapBodyQuery.visions Uuid's of selected visions
+ * @apiParam (body) {Boolean} MapBodyQuery.participate Defines if only projects should be return that have participations in the future
+ * @apiParam (body) {Number} [MapBodyQuery.skip=0] items to skip
+ * @apiParam (body) {Number} [MapBodyQuery.limit=10] items to skip
+ *
+ * @apiSuccess (200) {Object} Map200Query Success
+ * @apiSuccess (200) {Object[]} Map200Query.result Result
+ * @apiSuccess (200) {String} Map200Query.result.uuid Project uuid
+ * @apiSuccess (200) {String} Map200Query.result.title Project title
+ * @apiSuccess (200) {String} Map200Query.result.description Project description
+ * @apiSuccess (200) {Object} Map200Query.result.thumbnail Project thumbnail reference
+ * @apiSuccess (200) {Object} Map200Query.result.geoJSON Project geoJSON data
+ * @apiSuccess (200) {String[]} Map200Query.result.related Related projects (uuid)
+ * @apiSuccess (200) {String[]} Map200Query.result.childs Child projects (uuid)
+ * @apiSuccess (200) {Number} Map200Query.skip Skip
+ * @apiSuccess (200) {Object} Map200Query.limit Limit
+ * @apiSuccess (200) {Object} Map200Query.total Total items
+ *
+ * @apiError (400) {Object} Error Bad request
+ *
+ * @apiError (500) {Object} Error Internal server error
+ */
 module.exports.filtered = (req, res) => {
 
 };
--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -12,24 +12,28 @@
  * @apiParam (body) {Number} [ProjectsBodyQuery.skip=0] items to skip
  * @apiParam (body) {Number} [ProjectsBodyQuery.limit=10] items to skip
  *
- * @apiSuccess (200) {Object} Projects200Query. Success
- * @apiSuccess (200) {String} Projects200Query._id Mongo _id
- * @apiSuccess (200) {String} Projects200Query.uuid Content uuid
- * @apiSuccess (200) {Object} Projects200Query.fields All fields of the content
- * @apiSuccess (200) {Object} Projects200Query.meta Meta data of the content object
- * @apiSuccess (200) {String} Projects200Query.meta.label Content label
- * @apiSuccess (200) {Object} Projects200Query.meta.taxonomy Content taxonomy
- * @apiSuccess (200) {Array} Projects200Query.meta.taxonomy.available Taxonomy available
- * @apiSuccess (200) {String} Projects200Query.meta.taxonomy.dataType Taxonomy data type
- * @apiSuccess (200) {Array} Projects200Query.meta.taxonomy.tags Taxonomy tags
- * @apiSuccess (200) {Date} Projects200Query.meta.created Created at
- * @apiSuccess (200) {Date} Projects200Query.meta.lastModified Last modified at
- * @apiSuccess (200) {Boolean} Projects200Query.meta.published Published
- * @apiSuccess (200) {Number} Projects200Query.meta.hitCount Hit counter
- * @apiSuccess (200) {Array} Projects200Query.meta.activeLanguages Active languages
- * @apiSuccess (200) {String} Projects200Query.meta.contentType Content type
- * @apiSuccess (200) {String} Projects200Query.meta.safeLabel Safe label
- * @apiSuccess (200) {Boolean} Projects200Query.meta.deleted Deleted
+ * @apiSuccess (200) {Object} Projects200Query Success
+ * @apiSuccess (200) {Object[]} Projects200Query.result Success
+ * @apiSuccess (200) {String} Projects200Query.result._id Mongo _id
+ * @apiSuccess (200) {String} Projects200Query.result.uuid Content uuid
+ * @apiSuccess (200) {Object} Projects200Query.result.fields All fields of the content
+ * @apiSuccess (200) {Object} Projects200Query.result.meta Meta data of the content object
+ * @apiSuccess (200) {String} Projects200Query.result.meta.label Content label
+ * @apiSuccess (200) {Object} Projects200Query.result.meta.taxonomy Content taxonomy
+ * @apiSuccess (200) {Array} Projects200Query.result.meta.taxonomy.available Taxonomy available
+ * @apiSuccess (200) {String} Projects200Query.result.meta.taxonomy.dataType Taxonomy data type
+ * @apiSuccess (200) {Array} Projects200Query.result.meta.taxonomy.tags Taxonomy tags
+ * @apiSuccess (200) {Date} Projects200Query.result.meta.created Created at
+ * @apiSuccess (200) {Date} Projects200Query.result.meta.lastModified Last modified at
+ * @apiSuccess (200) {Boolean} Projects200Query.result.meta.published Published
+ * @apiSuccess (200) {Number} Projects200Query.result.meta.hitCount Hit counter
+ * @apiSuccess (200) {Array} Projects200Query.result.meta.activeLanguages Active languages
+ * @apiSuccess (200) {String} Projects200Query.result.meta.contentType Content type
+ * @apiSuccess (200) {String} Projects200Query.result.meta.safeLabel Safe label
+ * @apiSuccess (200) {Boolean} Projects200Query.result.meta.deleted Deleted
+ * @apiSuccess (200) {Number} Projects200Query.skip Skip
+ * @apiSuccess (200) {Number} Projects200Query.limit Limit
+ * @apiSuccess (200) {Number} Projects200Query.total Total
  *
  * @apiError (400) {Object} Error Bad request
  *
